b732e3b944c0c597fde91002b3521ee55d88f29f
kkdevopsb5onlie <dharimigariarjun@gmail.com>
2025-08-23 17:55:36 +0530

    Update building_image.groovy

--- vars/building_image.groovy
+++ vars/building_image.groovy
@@ -1,13 +1,16 @@
 def call(Map config = [:]) {
-    def imageName   = config.imageName ?: 'myorg/myapp'
-    def dockerfile  = config.dockerfile ?: 'Dockerfile'
-    def buildContext = config.buildContext ?: '.'
-
-    // generate timestamp tag
-    def TAG = sh(script: "date +%Y%m%d-%H%M%S", returnStdout: true).trim()
-    env.IMAGE_TAG = TAG
-
-    sh """
-        docker build -t ${imageName}:${env.IMAGE_TAG} -f ${dockerfile} ${buildContext}
-    """
+    if(config.imageName) {
+        def dockerfile  = config.dockerfile ?: 'Dockerfile'
+        def buildContext = config.buildContext ?: '.'
+    
+        // generate timestamp tag
+        def TAG = sh(script: "date +%Y%m%d-%H%M%S", returnStdout: true).trim()
+        env.IMAGE_TAG = TAG
+    
+        sh """
+            docker build -t ${imageName}:${env.IMAGE_TAG} -f ${dockerfile} ${buildContext}
+        """
+    } else {
+        throw new RuntimeException("Missing image name paramter..")
+    }
 }
