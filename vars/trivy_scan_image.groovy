3e467210f00828d4ce220fb09334cb2cd4831f8c
kkdevopsb5onlie <dharimigariarjun@gmail.com>
2025-08-24 13:02:07 +0530

    Update trivy_scan_image.groovy

--- vars/trivy_scan_image.groovy
+++ vars/trivy_scan_image.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
     def imageName   = config.imageName ?: '.'
     def format     = config.format ?: 'json'
-    def outputFile = config.outputFile ?: 'trivy-fs-report.json'
+    def outputFile = config.outputFile ?: 'trivy-image-scan-report.json'
 
     sh """
         trivy image -f ${format} -o ${outputFile} ${imageName}:${env.IMAGE_TAG}
